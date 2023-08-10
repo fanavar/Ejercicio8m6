85156b1b14e45e045c97419ed2e8210b4739c261
Fabián <fanavar@alumnos.ubiobio.cl>
2023-08-09 20:55:41 -0400

    avance

--- app/src/main/java/cl/awakelab/ejercicio8m6/MainActivity.kt
+++ app/src/main/java/cl/awakelab/ejercicio8m6/MainActivity.kt
@@ -8,14 +8,16 @@ App listado y detalle de perros
 [x] Manifest: activar permisos de internet, android:usesCleartextTraffic="true" y android:enableOnBackInvokedCallback="true"
 [x] Gradle: activar viewBinding | Plugin: kotlin-kapt | Dependencias: Coil, Room, RetroFit, navigation, viewModel |
    Se pasó a la version 17 en CompileOptions y KotlinOptions
+[x] Se creó la estructura de paquetes: data(local y remote) y view(correspondiente a la presentación)
+[X] Se creó la branch funcionalidad/consumo_remoto
 [] 2 pantallas: Listado y Detalle
 [] Navigation
 [] Listado: RecyclerView + ViewHolder + Adapter
 [] Detalle: RecyclerView + ViewHolder + Adapter
-[] Remoto:
-    [] DataClass
-    [] API (interfaz)
-    [] RetroFit
+[x] Remoto:
+    [x] DataClass
+    [x] API (interfaz)
+    [x] RetroFit
 [] Local:
     [] DataBase
     [] Entity
---
+++ app/src/main/java/cl/awakelab/ejercicio8m6/data/remote/RazaPerro.kt
@@ -0,0 +1,6 @@
+package cl.awakelab.ejercicio8m6.data.remote
+
+data class RazaPerro(
+    val message: Map<String, List<String>>,
+    val status: String
+)

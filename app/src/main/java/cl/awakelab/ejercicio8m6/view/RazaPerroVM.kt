9db0ae32a464d474173b598af68902d132a09a85
Fabián <fanavar@alumnos.ubiobio.cl>
2023-08-09 22:20:35 -0400

    view model

--- app/src/main/java/cl/awakelab/ejercicio8m6/view/RazaPerroVM.kt
+++ app/src/main/java/cl/awakelab/ejercicio8m6/view/RazaPerroVM.kt
@@ -1,4 +1,23 @@
 package cl.awakelab.ejercicio8m6.view
 
-class RazaPerroVM {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import cl.awakelab.ejercicio8m6.data.Repositorio
+import cl.awakelab.ejercicio8m6.data.local.RazaPerroDatabase
+import cl.awakelab.ejercicio8m6.data.remote.RazaPerroRetroFit
+import kotlinx.coroutines.launch
+
+class RazaPerroVM(application: Application): AndroidViewModel(application) {
+
+    private val repositorio: Repositorio
+    init {
+        val api = RazaPerroRetroFit.getRetrofitRazaPerro()
+        val razaPerroDatabase= RazaPerroDatabase.getDataBase(application).getRazaPerroDao()
+        repositorio = Repositorio(api, razaPerroDatabase)
+    }
+    fun getRazasPerro() = viewModelScope.launch {
+        repositorio.getRazasPerro()
+    }
+
 }
